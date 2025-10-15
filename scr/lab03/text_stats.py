395a47dac085d2450dd62a392f5752e7496dc213
erawue <goar3004@gmail.com>
2025-10-15 11:33:33 +0300

    j

--- scr/lab03/text_stats.py
+++ scr/lab03/text_stats.py
@@ -4,14 +4,11 @@ import os
 current_dir = os.path.dirname(__file__)
 lib_path = os.path.join(current_dir, '..', '..', 'lib')
 sys.path.append(lib_path)
-
 from text import normalize, top_n, tokenize, count_freq
-
 text = sys.stdin.read()
 words = tokenize(normalize(text))
 freq = count_freq(words)
 top = top_n(freq, 5)
-
 print(f'Всего слов: {len(words)}')
 print(f'Уникальных слов: {len(freq)}')
 print('Топ-5:')
